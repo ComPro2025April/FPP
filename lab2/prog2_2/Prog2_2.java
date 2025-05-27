aa80f031f4395fdcc863c16c5aa1bf5afd674a8c
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-05-27 17:34:44 -0500

    Done: Prog2_8 and Prog2_8Test

---
+++ lab2/prog2_2/Prog2_2.java
@@ -0,0 +1,13 @@
+package lab2.prog2_2;
+
+import static lab2.RandomNumbers.*;
+
+public class Prog2_2 {
+    public static void main(String[] args){
+        int x = getRandomInt(1,9);
+        int y = getRandomInt(3,14);
+
+        System.out.println("1. get a random number x in the range 1 .. 9 and compute πx : " + Math.pow(Math.PI,x));
+        System.out.println("2. get a random number y in the range 3 .. 14 and compute yπ : " + Math.pow(y,Math.PI));
+    }
+}
