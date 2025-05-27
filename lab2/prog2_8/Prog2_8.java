429ab4b2bc74bf6505f8467296af3feec7fd6eee
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-05-27 14:13:52 -0500

    Created lab directories and changed case

---
+++ lab2/prog2_8/Prog2_8.java
@@ -0,0 +1,15 @@
+package lab2.prog2_8;
+
+public class Prog2_8 {
+    public static void main(String[] args) {
+
+    }
+
+    static int min(int[] arrayOfInts){
+        int min = arrayOfInts[0];
+
+
+
+        return min;
+    }
+}
---
+++ lab2/prog2_8/Prog2_8Test.java
@@ -0,0 +1,4 @@
+package lab2.prog2_8;
+
+public class Prog2_8Test {
+}
