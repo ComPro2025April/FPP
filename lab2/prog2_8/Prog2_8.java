1b4366c8e8917d61e9c72352d28df1080d068574
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-05-27 14:26:48 -0500

    Done: Prog2_8 and Prog2_8Test

--- lab2/prog2_8/Prog2_8.java
+++ lab2/prog2_8/Prog2_8.java
@@ -1,14 +1,13 @@
 package lab2.prog2_8;
 
 public class Prog2_8 {
-    public static void main(String[] args) {
-
-    }
-
     static int min(int[] arrayOfInts){
         int min = arrayOfInts[0];
 
-
+        for(int i=1; i<arrayOfInts.length; i++){
+            if(arrayOfInts[i] < min)
+                min = arrayOfInts[i];
+        }
 
         return min;
     }
--- lab2/prog2_8/Prog2_8Test.java
+++ lab2/prog2_8/Prog2_8Test.java
@@ -1,4 +1,32 @@
 package lab2.prog2_8;
 
-public class Prog2_8Test {
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Prog2_8Tests {
+    @Test
+    public void testMin1() {
+        int[] arr = {2, -21, 3, 45, 0, 12, 18, 6, 3, 1, 0, -22};
+        int expected = -22;
+        int result = Prog2_8.min(arr);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMin2() {
+        int[] arr = {2, -21, 3, 45, 0, 12, 18, 6, 3, 1, 0, -23};
+        int expected = -23;
+        int result = Prog2_8.min(arr);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMin3() {
+        int[] arr = {2, -1, 0, 23};
+        int expected = -1;
+        int result = Prog2_8.min(arr);
+        assertEquals(expected, result);
+    }
 }
+
