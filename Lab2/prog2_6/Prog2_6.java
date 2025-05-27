d41599b58a8509db481ca6962ff7db4daa11052b
Abdelhadi Oumar <abdelhadioumar@gmail.com>
2025-05-27 08:42:55 -0500

    Add Lab2/prog2_6 directory

---
+++ Lab2/prog2_6/Prog2_6.java
@@ -0,0 +1,25 @@
+package Lab2.prog2_6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Prog2_6 {
+    public static String[] removeDups(String[] arr) {
+        String[] newArr = new String[arr.length];
+
+        List<String> newList = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!newList.contains(arr[i])) {
+                newList.add(arr[i]);
+            }
+        }
+
+        return newList.toArray(new String[0]);
+    }
+    public static void main(String[] args) {
+        String[] uniqueWords = {"horse", "dog", "cat", "horse","dog"};
+        System.out.println(Arrays.toString(removeDups(uniqueWords)));
+    }
+}
