0e316ebdbc28b1c1d34cc4490c6dd1e243f7ad3a
AbdelhadiOumar <abdelhadioumar@gmail.com>
2025-06-04 14:59:56 -0500

    Add lab7/prog7_4 directory

--- lab7/prog7_4/Prog7_4.java
+++ lab7/prog7_4/Prog7_4.java
@@ -1,7 +1,28 @@
 package lab7.prog7_4;
 
-public class Prog7_4 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Prog7_4 {
+
+    public static void reverseArray(int[] arr) {
+        reverseArray(arr, 0, arr.length - 1);
+    }
+
+    private static void reverseArray(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        reverseArray(arr, left + 1, right - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] data = {1, 3, 5, 7, 9, 13};
+        System.out.println("Before Reverse: " + Arrays.toString(data));
+        reverseArray(data);
+        System.out.println("After Reverse: " + Arrays.toString(data));
     }
 }
