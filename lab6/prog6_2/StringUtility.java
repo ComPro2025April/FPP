d2dbabde2060a1223f061ae892c713662adb95a0
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-06-04 14:14:44 -0500

    LAB6: Done porg 2

--- lab6/prog6_2/StringUtility.java
+++ lab6/prog6_2/StringUtility.java
@@ -2,25 +2,29 @@ package lab6.prog6_2;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class StringUtility extends JFrame{
     private JButton CountLetter;
     private JButton ReverseLetters;
     private JButton RemoveDuplicates;
-    private JTextField input;
-    private JTextField output;
+    private JTextField inputText;
+    private JTextField outputText;
     private JLabel inputLabel;
     private JLabel outputLabel;
     private JPanel panel;
 
     public StringUtility(){
         setTitle("String Utility");
-        input.setPreferredSize(new Dimension(200, 30));
-        output.setPreferredSize(new Dimension(200, 30));
+        inputText.setPreferredSize(new Dimension(200, 30));
+        outputText.setPreferredSize(new Dimension(200, 30));
+        outputText.setEditable(false);
+        outputText.setFocusable(false);
 
-/*        CountLetter.addActionListener(new CountLetterListener(text));
-        ReverseLetters.addActionListener(new ReverseLettersListener(text));
-        RemoveDuplicates.addActionListener(new RemoveDuplicatesListener(text));*/
+        CountLetter.addActionListener(new CountLetterListener(){});
+        ReverseLetters.addActionListener(new ReverseLettersListener(){});
+        RemoveDuplicates.addActionListener(new RemoveDuplicatesListener(){});
 
         add(panel);
         setSize(320,240);
@@ -28,6 +32,35 @@ public class StringUtility extends JFrame{
         setResizable(false);
         pack();
     }
+
+    class CountLetterListener implements ActionListener{
+        public void actionPerformed(ActionEvent evt){
+            outputText.setText(Integer.toString(inputText.getText().length()));
+        }
+    }
+
+    class ReverseLettersListener implements ActionListener{
+        public void actionPerformed(ActionEvent evt){
+            StringBuilder sb = new StringBuilder();
+            sb.append(inputText.getText());
+            outputText.setText(sb.reverse().toString());
+        }
+    }
+
+    class RemoveDuplicatesListener implements ActionListener{
+        public void actionPerformed(ActionEvent evt){
+            char[] tmp = inputText.getText().toCharArray();
+            StringBuilder sb = new StringBuilder();
+            String a = "";
+            for(char c : tmp){
+                if(sb.indexOf("" + c) == -1)
+                    sb.append(c);
+            }
+
+            outputText.setText(sb.toString());
+        }
+    }
+
 
     public static void centerFrameOnDesktop(Component f) {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
