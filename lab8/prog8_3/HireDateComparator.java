3a1e3e4f7d0ac8923cb531ab4f45ce5b1df61fb3
kshitij-grg <cheetizgurung@gmail.com>
2025-06-10 13:31:18 -0500

    Made comparatable consistent with equals

--- lab8/prog8_3/HireDateComparator.java
+++ lab8/prog8_3/HireDateComparator.java
@@ -5,12 +5,19 @@ import java.util.Date;
 
 public class HireDateComparator implements Comparator<Employee> {
     @Override
-    public int compare(Employee o1, Employee o2) {
-        Date hireDate1 = o1.getHireDate(), hireDate2 = o2.getHireDate();
+    public int compare(Employee e1, Employee e2) {
+        Date hireDate1 = e1.getHireDate(), hireDate2 = e2.getHireDate();
+        int salary1 = e1.getSalary(), salary2 = e2.getSalary();
+        String name1 = e1.getName(), name2 = e2.getName();
 
         if (hireDate1.compareTo(hireDate2) != 0) {
             return hireDate1.compareTo(hireDate2);
         }
-        return 0;
+
+        if (name1.compareTo(name2) != 0) {
+            return name1.compareTo(name2);
+        }
+
+        return Integer.compare(salary1, salary2);
     }
 }
--- lab8/prog8_3/SalaryComparator.java
+++ lab8/prog8_3/SalaryComparator.java
@@ -1,11 +1,23 @@
 package lab8.prog8_3;
 
 import java.util.Comparator;
+import java.util.Date;
 
 public class SalaryComparator implements Comparator<Employee> {
     @Override
-    public int compare(Employee o1, Employee o2) {
-        int salary1 = o1.getSalary(), salary2 = o2.getSalary();
-        return Integer.compare(salary1, salary2);
+    public int compare(Employee e1, Employee e2) {
+        int salary1 = e1.getSalary(), salary2 = e2.getSalary();
+        String name1 = e1.getName(), name2 = e2.getName();
+        Date hireDate1 = e1.getHireDate(), hireDate2 = e2.getHireDate();
+
+        if (salary1 != salary2) {
+            return Integer.compare(salary1, salary2);
+        }
+
+        if (name1.compareTo(name2) != 0) {
+            return name1.compareTo(name2);
+        }
+
+        return hireDate1.compareTo(hireDate2);
     }
 }
