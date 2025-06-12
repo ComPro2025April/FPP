45d21165672e0279b4b86f8e1e56191be7103c7a
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-06-12 15:41:45 -0500

    Lab10: Done

--- lab10/prog10_1/employeebst/EmployeeDriver.java
+++ lab10/prog10_1/employeebst/EmployeeDriver.java
@@ -10,6 +10,14 @@ public class EmployeeDriver {
 	public EmployeeDriver() {
 		NameComparator nameComp = new NameComparator();
 		EmployeeBST bst = new EmployeeBST(nameComp);
+/*		bst.insert(new Employee("C", 40000, 1996,11,5));
+		bst.insert(new Employee("E", 80000, 1986,3,23));
+		bst.insert(new Employee("D", 50000, 2000, 1, 3));
+		bst.insert(new Employee("A", 45000, 2001, 2, 7));
+		bst.insert(new Employee("B", 80000, 1986,3,23));
+		bst.insert(new Employee("F", 80000, 1986,3,23));*/
+
+
 		bst.insert(new Employee("C", 40000, 1996,11,5));
 		bst.insert(new Employee("E", 80000, 1986,3,23));
 		bst.insert(new Employee("D", 50000, 2000, 1, 3));
