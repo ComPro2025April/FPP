997ea484bc9cb9d7a8fde3e5761400cb6887f4a8
kshitij-grg <cheetizgurung@gmail.com>
2025-06-16 14:06:45 -0500

    Minsort recursive

--- lab7/prog7_2/MinSort.java
+++ lab7/prog7_2/MinSort.java
@@ -1,35 +1,55 @@
 package lab7.prog7_2;
 
+import java.util.Arrays;
+
 public class MinSort {
+//    this one works too
+//    public String sort(String s) {
+//        if (s == null || s.isEmpty()) return s;
+//        char min = s.charAt(0);
+//        int minPos = 0;
+//        for (int i = 1; i < s.length(); i++) {
+//            char tempChar = s.charAt(i);
+//            if (tempChar < min) {
+//                min = tempChar;
+//                minPos = i;
+//            }
+//        }
+//        String remaining = s.substring(0, minPos) + s.substring(minPos + 1);
+//        return min + sort(remaining);
+//    }
+
     public String sort(String s) {
-        if (s.isEmpty()) return null;
-        char[] sArray = s.toCharArray();
-        int len = sArray.length;
+        if (s == null || s.length() <= 1) return s;
 
-        for (int i = 0; i < len; i++) {
-            int minIndex = minPos(i, len - 1, sArray);
-            swap(i, minIndex, sArray);
-        }
+        int minPos = findMinPos(s);
 
-        return new String(sArray);
+        // Swap character at index 0 with the minimum character
+        char minChar = s.charAt(minPos);
+        char firstChar = s.charAt(0);
+
+        // Build new string with swapped characters
+        String swapped = minChar + s.substring(1, minPos) + firstChar + s.substring(minPos + 1);
+
+        // Separate first character (minChar) and remaining string
+        String remaining = swapped.substring(1); // remove first character (minChar)
+
+        // Recursive call on the remaining string
+        return minChar + sort(remaining);
     }
 
-    public void swap(int i, int j, char[] sArray) {
-        char temp = sArray[i];
-        sArray[i] = sArray[j];
-        sArray[j] = temp;
-    }
+    // Finds the position of the smallest character in the string
+    private int findMinPos(String s) {
+        char minChar = s.charAt(0);
+        int minPos = 0;
 
-    public int minPos(int left, int right, char[] sArray) {
-        int index = left;
-
-        for (int i = left + 1; i <= right; i++) {
-            if (sArray[i] < sArray[index]) {
-                index = i;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) < minChar) {
+                minChar = s.charAt(i);
+                minPos = i;
             }
         }
-        return index;
+
+        return minPos;
     }
-
-
 }
